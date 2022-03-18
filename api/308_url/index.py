e0839d4449d19658b1277975498fba1ae0c03b16
Zkeq <learnonly@learnonly.xyz>
2022-03-18 10:44:07 +0800

    优化日志

--- api/308_url/index.py
+++ api/308_url/index.py
@@ -18,25 +18,37 @@ r = redis.Redis(
 
 def get_video(_url, _cache, url_form):
     _str_base64 = base64.b64decode(url_form)
+    print("_str_base64:", _str_base64)
     data = r.get(_url)
+    print("try get data:", data)
     if data is None:
+        print("data is None")
         data = requests.get(_url).content
         r.set(_url, data, ex=_cache)
         data = json.loads(data)
+        print("data:", data)
     else:
         data = data.decode('utf-8')
         data = json.loads(data)
+        print("data:", data)
     data_content = eval(_str_base64)
+    print("data_content:", data_content)
     return data_content
 
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
+        print("self.path:", self.path)
         _form = self.path.split('?')[1]
+        print('_form:', _form)
         url = unquote(_form.split('url=')[1]).split('&')[0]
+        print('url:', url)
         cache = _form.split('cache=')[1].split('&')[0]
+        print('cache:', cache)
         _url_form = _form.split('form=')[1].split('&')[0]
+        print('_url_form:', _url_form)
         params_data = get_video(url, cache, _url_form)
+        print('params_data:', params_data)
         self.send_response(308)  # vercel 只有 308 跳转才可以缓存 详情见官方文档
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('location', params_data)  # 这个是主要的
