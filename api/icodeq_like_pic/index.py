55d80efe98fc606490c0e175b4ecd25e24b7d980
zkeq <admin@icodeq.com>
2022-05-17 00:53:25 +0800

    hour

--- api/icodeq_like_pic/index.py
+++ api/icodeq_like_pic/index.py
@@ -11,8 +11,9 @@ pic_list = [
 def get_pic():
     # 获取当前的秒数
     now_time = time.time()
-    # 获取当前的秒数的余数
-    now_time_mod = int(now_time % len(pic_list))
+    # 获取当前的小时数
+    now_hour = time.localtime(now_time).tm_hour
+    now_time_mod = int(now_hour % len(pic_list))
     # 获取当前的秒数的余数的图片地址
     pic_url = pic_list[now_time_mod]
     return pic_url
@@ -25,7 +26,7 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('location', url) # 这个是主要的
         self.send_header('Refresh', '0;url={}'.format(url))
-        # self.send_header('Cache-Control', 'max-age=0, s-maxage=60, stale-while-revalidate=3600') # vercel 缓存
+        self.send_header('Cache-Control', 'max-age=0, s-maxage=60, stale-while-revalidate=3600') # vercel 缓存
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
         self.wfile.write('Redirecting to {} (308)'.format(url).encode('utf-8'))  # 这里无所谓
