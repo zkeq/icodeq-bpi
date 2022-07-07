19909609184a9f6c26e0a2ae0f83e74e19a7499a
zkeq <admin@icodeq.com>
2022-07-07 21:42:54 +0800

    del 【】

--- api/news_163/crawler.py
+++ api/news_163/crawler.py
@@ -23,6 +23,7 @@ def get_days(index):
     for i in list_all:
         if "<" not in i and ">" not in i and i != '':
             i.replace('\u200b', '')
+            i.replace('【微语】', '')
             if '、' in i:
                 new_str = '、'.join(i.split('、')[1:])
                 news_list.append(new_str)
